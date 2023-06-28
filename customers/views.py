8315c1b4aba40d3854ef660d06aa49dd4a463142
Kacper <kacperodziemczyk@gmail.com>
2023-06-28 12:42:53 +0200

    JWT authentication webTokens

--- customers/views.py
+++ customers/views.py
@@ -1,10 +1,12 @@
 from customers.models import Customer
 from django.http import JsonResponse,Http404
 from customers.serializers import CustomerSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 @api_view(['GET', 'POST' ])
+@permission_classes([IsAuthenticated])
 def customers(request):
     if request.method =='GET':
         data = Customer.objects.all()
@@ -16,8 +18,9 @@ def customers(request):
             serializer.save()
             return Response({'customer':serializer.data},status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'POST', 'DELETE'])
+    
+@api_view(['GET', 'POST', 'DELETE'])    
+@permission_classes([IsAuthenticated])
 def customer(request, id):
     try:
         data = Customer.objects.get(pk=id)
